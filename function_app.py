d55b5ef7325eb51b2c999da7c96a1f514f16f9b6
antidoid <singhpiyush998@gmail.com>
2024-03-13 17:11:26 +0530

    Fix: Use managed identity

--- function_app.py
+++ function_app.py
@@ -1,4 +1,6 @@
 import azure.functions as func
+import json
+from azure.identity import DefaultAzureCredential
 
 app = func.FunctionApp()
 
@@ -10,9 +12,16 @@ app = func.FunctionApp()
     container_name="resumes",
     id="{id}",
     partition_key="{id}",
-    connection="CosmosDbConnectionSetting"
+    connection="connect"
 )
 def GetResumeData(req: func.HttpRequest, resume: func.DocumentList) -> func.HttpResponse:
+    if not resume:
+        return func.HttpResponse(
+            status_code=404,
+            mimetype="application/json",
+            body=json.dumps(
+                {"message": "Resume not found, please try a valid resume id"})
+        )
     return func.HttpResponse(
         status_code=200,
         mimetype="application/json",
