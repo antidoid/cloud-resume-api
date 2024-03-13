07d35de4ffaba060c121acb3039b235f61a9705d
antidoid <singhpiyush998@gmail.com>
2024-03-13 11:15:30 +0530

    Feat: Connect to cosmos db

--- function_app.py
+++ function_app.py
@@ -1,26 +1,39 @@
 import azure.functions as func
-import datetime
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient
 import json
 import logging
 
 app = func.FunctionApp()
 
-@app.route(route="GetResumeData", auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="GetResumeData/{id:int}", auth_level=func.AuthLevel.ANONYMOUS)
 def GetResumeData(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-    
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    # Get the resume id from route params
+    id = req.route_params.get('id')
+    if not id:
+        return func.HttpResponse("Missing id in the function url")
+
+    # Get the resume data from comosdb nosql api
+    logging.info(f"Fetching resume data for id: {id}")
+    client = CosmosClient(
+        url="https://cloud-resume-api-dbacc.documents.azure.com:443",
+        credential=DefaultAzureCredential()
+    )
+    resume_db = client.get_database_client("resume-db")
+    container = resume_db.get_container_client("resumes")
+
+    try:
+        resume = container.read_item(item=id, partition_key=id)
+    except:
+        logging.warn("Cannot find a resume with that id")
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
+            status_code=404,
+            body="Error finding resume with the given id, please provide a valid resume id"
         )
+
+    return func.HttpResponse(
+        status_code=200,
+        mimetype="application/json",
+        body=json.dumps(resume),
+    )
